16133c96b3e4ec90f2e1475bb685de6016a43ad2
Devang Borkar <devangborkar3@gmail.com>
2025-04-18 05:56:20 -0700

    Fix building errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: {},
-  },
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser',
     },
-  }
+  },
+  optimizeDeps: {
+    include: ['@chakra-ui/react', '@chakra-ui/theme'],
+  },
 })
